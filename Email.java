f8e29b6f9a40e46bb3f366bfb3b6248c429be452
programmerAju <mmohammedajmal7@gmail.com>
2024-06-29 22:13:45 +0530

    add fields

---
+++ Email.java
@@ -0,0 +1,21 @@
+class Email{
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String department;
+    private int mailboxCapacity;
+    private String alternateEmail;
+
+    // Constructor to recieve the first name and last name 
+
+    // Ask for the department
+
+    // Generate the random password
+
+    // Set the mailbox capacity
+
+    // Set the alternate email address
+
+    // change the password
+    
+}
