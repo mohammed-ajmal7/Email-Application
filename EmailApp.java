b6e9df32f1ac7b09034c9f38ed6979fbe1d433c9
programmerAju <mmohammedajmal7@gmail.com>
2024-06-30 10:18:31 +0530

    finish

--- EmailApp.java
+++ EmailApp.java
@@ -1,5 +1,16 @@
 public class EmailApp {
     public static void main(String[] args) {
+        Email em1 = new Email("John", "Wick");
+
+        // em1.setAlternateEmail("mmmohammedajmal7@gmail.com");
+        // System.out.println("The Alternate Email :" + em1.getAlternateEmail());
         
+        // em1.setMailboxCapacity(3000);
+        // System.out.println("the new mailbox capacity :" + em1.getMailboxCapacity());
+        
+        // em1.setNewPassword("MakeYourselfBetter@82759");
+        // System.out.println("The new password :" + em1.getNewPassword());
+
+        System.out.println(em1.showInfo());
     }
 }
